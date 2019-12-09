9b979d219a3939ad4ad6ea2796f90e462b80e4f9
samwisegamgee <hassanabdulmajig@gmail.com>
2019-12-09 10:17:16 -0700

    lesson-02

--- app/src/main/java/www/pickens/whatscool/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/www/pickens/whatscool/coderswag/Controller/MainActivity.kt
@@ -1,13 +1,27 @@
 package www.pickens.whatscool.coderswag.Controller
 
+import android.content.AbstractThreadedSyncAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import www.pickens.whatscool.coderswag.Model.Category
 import www.pickens.whatscool.coderswag.R
+import www.pickens.whatscool.coderswag.Services.DataService
+
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    lateinit var  adapter: ArrayAdapter<Category>
+
+            override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+                adapter = ArrayAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                   DataService.Categories)
+
+                catogoryListview.adapter = adapter
     }
 }
